f760d7d290ed70b708fe7efe7300b49d5425766b
Joseph Adeyemo <j_adeyemo@yahoo.com>
2021-06-09 14:14:25 -0400

    6/9/2021 2:14PM

--- src/App.js
+++ src/App.js
@@ -1,23 +1,108 @@
-import logo from './logo.svg';
+import React, {useState} from 'react'
 import './App.css';
+import Length from './components/Length'
 
 function App() {
+
+  const [displayTime, setDisplayTime] = useState(25 * 60);
+  const [breakTime, setBreakTime] = useState(5 * 60);
+  const [sessionTime, setSessionTime] = useState(25 * 60);
+  const [timerOn, setTimerOn] = useState(false);
+  const [onBreak, setOnBreak] = useState(false);
+
+
+
+  const changeTime = (amount, type) => {
+    if (type === "break"){
+      if(breakTime <= 60 && amount < 0){
+        return;
+      }
+      setBreakTime(prev => prev + amount);
+    } else {
+      if(sessionTime <= 60 && amount < 0){
+        return;
+      }
+      setSessionTime(prev => prev + amount);
+      if(!timerOn) {
+        setDisplayTime(sessionTime + amount)
+      }
+    }
+  }
+
+
+  const formatTime = (time) => {
+    let minutes = Math.floor(time/60);
+    let seconds = time % 60;
+
+    return (
+      (minutes < 10 ? "0" + minutes : minutes) + ":" + (seconds < 10 ? "0" + seconds : seconds)
+    )
+  }
+
+  const controlTime = () => {
+    let second = 1000;
+    let date = new Date().getTime();
+    let nextDate = new Date().getTime() + second;
+    let onBreakVariable = onBreak;
+    if(!timerOn) {
+    let interval = setInterval(() => {
+      date = new Date().getTime();
+      if(date > nextDate) {
+        setDisplayTime((prev) => {
+          if(prev <= 0 && !onBreakVariable){
+            onBreakVariable = true;
+            setOnBreak(true);
+            return breakTime
+          } else if(prev <= 0 && onBreakVariable){
+            onBreakVariable = true;
+            setOnBreak(true);
+            return sessionTime
+          }
+          return prev - 1;
+        });
+        nextDate += second;
+      }
+    }, 30);
+    localStorage.clear()
+    localStorage.setItem('interval-id', interval)
+  }
+
+  if(timerOn) {
+    clearInterval(localStorage.getItem('interval-id'));
+  }
+
+  setTimerOn(!timerOn)
+  }
+
+
+  const resetTime = () => {
+    setDisplayTime(25 * 60);
+    setBreakTime(5 * 60);
+    sessionTime(25 * 60);
+  }
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="center-align">
+      <h1>Pomodoro Clock</h1>
+      <div className="dual-container">
+      <Length title={'break length'} changeTime={changeTime} type={"break"} time={breakTime} formatTime={formatTime}/>
+      <Length title={'session length'} changeTime={changeTime} type={"session"} time={sessionTime} formatTime={formatTime}/>
+      </div>
+      <h3>{onBreak ? 'Break' : 'Session'}</h3>
+        <h1>{formatTime(displayTime)}</h1> 
+        <button className="btn-large deep-purple lighten-2" onClick={controlTime}>
+          {timerOn ? (
+            <i className="material-icons">pause_circle_filled</i>
+          ) : (
+            <i className="material-icons">play_circle_filled</i>
+          )}
+        </button>
+        <button className="btn-large deep-purple lighten-2" onClick={resetTime}>
+            <i className="material-icons">autorenew</i>
+        </button>
     </div>
   );
 }
